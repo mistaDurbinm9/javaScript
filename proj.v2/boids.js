1af86b4de4815c7bf7cf6fa7065a31259defa827
unknown <tdavis@WS-I3-33.WS-ACADBUS.EDU>
2016-09-23 12:28:56 -0700

    lol

--- proj.v2/boids.js
+++ proj.v2/boids.js
@@ -13,51 +13,64 @@ function Boid() {
     ellipse(this.loc.x, this.loc.y, 10, 10);
 
   }
-  
-  
+
+
 
   this.update = function(force) {
-      this.force = force;
-      this.force2 = force;
-      
-      this.fear = random(100, 200);
-      
-      this.force = p5.Vector.sub(this.loc, r.loc);
-      this.force2 = p5.Vector.sub(this.loc, a.loc);
-      
-      this.force.normalize();
-      this.force.mult(.1);
-      
-      this.force2.normalize();
-      this.force2.mult(.1);
-      
-      if (this.loc.dist(r.loc) < 50) {
-        line(this.loc.x, this.loc.y, r.loc.x, r.loc.y);
-        this.applyForce(this.force);
-        this.vel.add(this.force);
-        this.vel.limit(random(3, 6));
-      } else if (this.loc.dist(r.loc) < 90) {
-        line(this.loc.x, this.loc.y, r.loc.x, r.loc.y);
-        this.applyForce(this.force);
-        this.vel.add(this.force);
-        this.vel.limit(random(1, 2));
-      } else if (this.loc.dist(a.loc) < 100) {
-        line(this.loc.x, this.loc.y, a.loc.x, a.loc.y);
-        this.applyForce(this.force2);
-        this.vel.add(this.force2.mult(-2));
-        this.vel.limit(random(3, 6));
-      } else if (this.loc.dist(a.loc) < 90) {
-        this.applyForce(this.force2);
-        this.vel.add(this.force2.mult(-1));
-        this.vel.limit(random(1, 2));
-      } else {
-        this.vel.limit(1);
-      }
-      this.loc.add(this.vel);
-      this.acc.mult(0);
-      this.checkEdges();
+    this.force = force;
+    this.force2 = force;
+
+    this.fear = random(100, 200);
+
+    this.force = p5.Vector.sub(this.loc, r.loc);
+    this.force2 = p5.Vector.sub(this.loc, a.loc);
+
+    this.force.normalize();
+    this.force.mult(.1);
+
+    this.force2.normalize();
+    this.force2.mult(.1);
+
+    if (this.loc.dist(r.loc) < 50) {
+      push();
+      stroke(255, 0, 0);
+      line(this.loc.x, this.loc.y, r.loc.x, r.loc.y);
+      pop();
+      this.applyForce(this.force);
+      this.vel.add(this.force);
+      this.vel.limit(random(3, 6));
+    } else if (this.loc.dist(r.loc) < 90) {
+      push();
+      stroke(255, 0, 0);
+      line(this.loc.x, this.loc.y, r.loc.x, r.loc.y);
+      pop();
+      this.applyForce(this.force);
+      this.vel.add(this.force);
+      this.vel.limit(random(1, 2));
+    } else if (this.loc.dist(a.loc) < 100) {
+      push();
+      stroke(0, 255, 0);
+      line(this.loc.x, this.loc.y, a.loc.x, a.loc.y);
+      pop();
+      this.applyForce(this.force2);
+      this.vel.add(this.force2.mult(-2));
+      this.vel.limit(random(3, 6));
+    } else if (this.loc.dist(a.loc) < 90) {
+      push();
+      stroke(0, 255, 0);
+      line(this.loc.x, this.loc.y, a.loc.x, a.loc.y);
+      pop();
+      this.applyForce(this.force2);
+      this.vel.add(this.force2.mult(-1));
+      this.vel.limit(random(1, 2));
+    } else {
+      this.vel.limit(1);
     }
-    
+    this.loc.add(this.vel);
+    this.acc.mult(0);
+    this.checkEdges();
+  }
+
   this.checkEdges = function() {
     if (this.loc.x > width || this.loc.x < 0) this.vel.x *= -1;
     if (this.loc.y > height || this.loc.y < 0) this.vel.y *= -1;
